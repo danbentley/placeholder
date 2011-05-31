7bc44da9b054126217d9fb4e60c3fc1cc3d40287
Dan Bentley <danbentley9@gmail.com>
2011-05-31 22:21:56 +0100

    Fixed issue where an inputs value could never be the same as the placeholder text.

--- js/jquery.placeholder.js
+++ js/jquery.placeholder.js
@@ -1,40 +1,50 @@
 (function($) {
 
-    /**
-     * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
-     * 
-     * Copyright 2011 Dan Bentley
-     * Licensed under the Apache License 2.0
-     *
-     * Author: Dan Bentley [github.com/danbentley]
-     */
+	/**
+	 * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
+	 * 
+	 * Copyright 2011 Dan Bentley
+	 * Licensed under the Apache License 2.0
+	 *
+	 * Author: Dan Bentley [github.com/danbentley]
+	 */
 
-    // Return if native support is available.
-    if ("placeholder" in document.createElement("input")) return;
+	// Return if native support is available.
+	if ("placeholder" in document.createElement("input")) return;
 
-    function setupPlaceholder(input) {
-
-        var placeholderText = input.attr('placeholder');
-
-        if (input.val() === '') input.val(placeholderText);
-        input.bind('focus blur', function(e) {
-            if (e.type === 'focus' && input.val() === placeholderText) input.val(''); 
-            if (e.type === 'blur' && input.val() === '') input.val(placeholderText); 
-        });
-    }
-
-    function clearPlaceholdersBeforeSubmit(form) {
-        form.find(':input[placeholder]').each(function() {
-            var el = $(this);
-            if (el.val() === el.attr('placeholder')) el.val('');
-        });
-    }
- 
-    $(':input[placeholder]').each(function(index) {
-        setupPlaceholder($(this));
-    });
+	$(':input[placeholder]').each(function(index) {
+		setupPlaceholder($(this));
+	});
    
-    $('form').submit(function(e) {
-        clearPlaceholdersBeforeSubmit($(this));
-    });
+	$('form').submit(function(e) {
+		clearPlaceholdersBeforeSubmit($(this));
+	});
+
+	function setupPlaceholder(input) {
+
+		var placeholderText = input.attr('placeholder');
+
+		setPlaceholderOrFlagChanged(input, placeholderText);
+		input.bind('focus blur change', function(e) {
+			if (e.type === 'focus' && input.val() === placeholderText && !input.data('changed')) input.val('');
+			if (e.type === 'blur' && input.val() === '') input.val(placeholderText);
+			if (e.type === 'change' && input.val() !== '') input.data('changed', true);  
+		});
+	}
+
+	function setPlaceholderOrFlagChanged(input, placeholderText) {
+
+		if (input.val() === '') {
+			input.val(placeholderText);
+		} else {
+			input.data('changed', true);
+		}
+	}
+
+	function clearPlaceholdersBeforeSubmit(form) {
+		form.find(':input[placeholder]').each(function() {
+			var el = $(this);
+			if (el.val() === el.attr('placeholder') && !el.data('changed')) el.val('');
+		});
+	}
 })(jQuery);
