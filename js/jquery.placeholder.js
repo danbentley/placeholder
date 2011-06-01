d3c29b2c723adaaacbcc142114a0c9ca465cabf3
Dan Bentley <danbentley9@gmail.com>
2011-06-01 23:59:21 +0100

    Cleaned up code. bind() now uses a map of event type/handler pairs.

--- js/jquery.placeholder.js
+++ js/jquery.placeholder.js
@@ -1,40 +1,44 @@
 (function($) {
 
-    /**
-     * Spoofs placeholders in browsers that don't support them. (eg Firefox 3)
-     * 
-     * Copyright 2011 Dan Bentley
-     * Licensed under the Apache License 2.0
-     *
-     * Author: Dan Bentley [github.com/danbentley]
-     */
+	/**
+	 * Spoofs placeholders in browsers that don't support them (eg Firefox 3)
+	 * 
+	 * Copyright 2011 Dan Bentley
+	 * Licensed under the Apache License 2.0
+	 *
+	 * Author: Dan Bentley [github.com/danbentley]
+	 */
 
-    // Return if native support is available.
-    if ("placeholder" in document.createElement("input")) return;
+	// Return if native support is available.
+	if ("placeholder" in document.createElement("input")) return;
 
-    function setupPlaceholder(input) {
-
-        var placeholderText = input.attr('placeholder');
-
-        if (input.val() === '') input.val(placeholderText);
-        input.bind('focus blur', function(e) {
-            if (e.type === 'focus' && input.val() === placeholderText) input.val(''); 
-            if (e.type === 'blur' && input.val() === '') input.val(placeholderText); 
-        });
-    }
-
-    function clearPlaceholdersBeforeSubmit(form) {
-        form.find(':input[placeholder]').each(function() {
-            var el = $(this);
-            if (el.val() === el.attr('placeholder')) el.val('');
-        });
-    }
- 
-    $(':input[placeholder]').each(function(index) {
-        setupPlaceholder($(this));
-    });
+	$(':input[placeholder]').each(function() {
+		setupPlaceholder($(this));
+	});
    
-    $('form').submit(function(e) {
-        clearPlaceholdersBeforeSubmit($(this));
-    });
+	$('form').submit(function(e) {
+		clearPlaceholdersBeforeSubmit($(this));
+	});
+
+	function setupPlaceholder(input) {
+
+		var placeholderText = input.attr('placeholder');
+
+		if (input.val() === '') input.val(placeholderText);
+		input.bind({
+			focus: function(e) {
+				if (input.val() === placeholderText) input.val('');
+			},
+			blur: function(e) {
+				if (input.val() === '') input.val(placeholderText); 
+			}
+		});
+	}
+
+	function clearPlaceholdersBeforeSubmit(form) {
+		form.find(':input[placeholder]').each(function() {
+			var el = $(this);
+			if (el.val() === el.attr('placeholder')) el.val('');
+		});
+	}
 })(jQuery);
